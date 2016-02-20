1bcda5af72d668d84d2b0732eb2acd826ce811ce
Quinn Spicker <quinnspicker@gmail.com>
2016-02-20 22:15:11 +0000

    style errors and messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "article was successfully created"
+      flash[:success] = "article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was deleted"
+    flash[:danger] = "Article was deleted"
     redirect_to articles_path
   end
 
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "article successfully updated"
+      flash[:success] = "article successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
